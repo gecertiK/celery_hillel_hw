3204a17d1aa41e79ccc26e11d77af654396d36f7
David(gecert) <dkarandze4@gmail.com>
2022-08-15 10:50:21 +0200

    create code to parse quotes

--- celery_application/models.py
+++ celery_application/models.py
@@ -1,3 +1,19 @@
 from django.db import models  # noqa:F401
 
+
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, default="Some String")
+    description = models.CharField(max_length=1000000, default="Some String")
+
+    def __str__(self):
+        return self.name
+
+
+class Quotes(models.Model):
+    texts = models.CharField(max_length=255, default="Some String")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.texts
