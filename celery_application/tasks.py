0d174455cd3e5634d09a60dbd4f8915fe036943c
David(gecert) <dkarandze4@gmail.com>
2022-08-21 15:59:15 +0300

    parse_quotes

--- celery_application/tasks.py
+++ celery_application/tasks.py
@@ -24,29 +24,26 @@ def work_done():
 @shared_task
 def parse_quotes():
     count = 0
-    url = 'https://quotes.toscrape.com/'
-    quote_list = []
-    while count < 5:
-        get_url = requests.get(url)
-        beautifulsoup_html = BeautifulSoup(get_url.content, 'html.parser')
-        quote_db = Quotes.objects.values('texts')
-        quotes = beautifulsoup_html.find_all("div", {"class": "quote"})
-        for _ in quote_db:
-            quote_list.append(['texts'])
-        for get_quote in quotes:
-            if get_quote.span.text not in quote_list:
-                authors = get_quote.small.text
-                url_description = requests.get(url + get_quote.a.get('href'))
-                soup_description = BeautifulSoup(url_description.content, 'html.parser')
-                auth_description = soup_description.find("div", {"class": "author-description"})
-                author = Author.objects.get_or_create(name=authors, defaults={'description': auth_description.text})
-                Quotes.objects.get_or_create(message=get_quote.span.text, author=author[0])
+    while True:
+        url = 'https://quotes.toscrape.com/'
+        r = requests.get(url)
+        s = BeautifulSoup(r.content, 'html.parser')
+        quotes = s.find_all("div", {"class": "quote"})
+        for i in quotes:
+            if i.span.text not in Quotes.objects.filter(message=i.span.text).exists():
+                author = i.small.text
+                description_url = requests.get(url + i.a.get('href'))
+                soup_description = BeautifulSoup(description_url.content, 'html.parser')
+                description = soup_description.find("div", {"class": "author-description"})
+                author1 = Author.objects.get_or_create(name=author, defaults={'description': description.text})
+                Quotes.objects.create(message=i.span.text, author=author1[0])
                 count += 1
                 if count == 5:
                     break
-            if get_quote.span.text or count < 5 not in quote_list:
-                next_page = beautifulsoup_html.find("li", {"class": "next"}).a.get("href")
-                url = url + next_page
-            else:
-                url = None
-                work_done.delay()
+        if count < 5:
+            next_url = s.find("li", {"class": "next"}).a.get("href")
+            url = url + next_url
+            if next_url is None:
+                send_email("Quotes have ended!", "The are not new quotes in this site!", "admin@test.com",
+                                 ['admin@example.com'])
+                break
